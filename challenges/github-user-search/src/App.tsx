3154e119db8d044aa98f89215313b415277cfc01
Kacper Kłosowski <kacpermklosowski@gmail.com>
2024-01-06 21:26:39 +0100

    refactor mocked user data

--- challenges/github-user-search/src/App.tsx
+++ challenges/github-user-search/src/App.tsx
@@ -4,41 +4,10 @@ import SearchBar from "./components/SearchBar";
 import UserProfile from "./components/UserProfile";
 import {UserData} from "./types/UserData.types.ts";
 import './App.scss'
-
-import mockedUserProfile from "./data/mockedUserProfile.ts";
-
-const {
-    twitter_username,
-    following,
-    created_at,
-    company,
-    name,
-    bio,
-    public_repos,
-    followers,
-    avatar_url,
-    location,
-    blog,
-    login
-} = mockedUserProfile;
-
-const initialUserData: UserData = {
-    avatarUrl: avatar_url,
-    bio: bio,
-    blog: blog,
-    company: company,
-    createdAt: created_at,
-    followers: followers,
-    following: following,
-    location: location,
-    login: login,
-    name: name,
-    publicRepos: public_repos,
-    twitterUsername: twitter_username
-};
+import {mockedUserData} from "./data/mockedUserData.ts";
 
 export default function App() {
-    const [userData, _] = useState<UserData>(initialUserData);
+    const [userData, _] = useState<UserData>(mockedUserData);
     return (
         <div>
             <Navigation/>
